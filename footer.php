88f547fa12f60f7be1fd9945c3f3de567c45297e
ssohamdes <ssoham.des@gmail.com>
2024-02-13 08:08:54 +0530

    Initial commit

---
+++ footer.php
@@ -0,0 +1,12 @@
+<div style="position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: rgba(0,0,0,0.8);
+    color: white;
+    text-align: center;
+    padding: 10px 0; /* Adjust as needed */
+    z-index: 1000; /* Ensure it's above other content */
+">
+  <h4>&copy; <b>Crime Portal 2024</b></h4>
+</div>
